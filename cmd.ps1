9607565fddb6f692d4a230450e157545a88f917e
saturnino <161881@unsaac.edu.pe>
2025-05-21 07:09:31 -0500

    .NET provides a fast a modular platform for creating many different types

---
+++ cmd.ps1
@@ -0,0 +1,8 @@
+Write-Output "git-init create an empty GIT repository or reinitialize an existing one"
+git init && git add -A
+
+Write-Host "git init && git add -A && git commit -m "...""
+
+Write-Host "git branch -M main && git remote add origin Url && git pull origin main --rebase && git push -u origin main "
+
+
